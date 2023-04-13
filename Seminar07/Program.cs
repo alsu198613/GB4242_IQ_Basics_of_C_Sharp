ea126d66d996f85662e8e9bc60c1a1a6261cfddd
Alexandra Sukneva <alsu198613@gmail.com>
2023-04-13 22:18:04 +0300

    updated seminar7

--- Seminar07/Program.cs
+++ Seminar07/Program.cs
@@ -1,2 +1,203 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m + n.
+Выведите полученный массив на экран.
+m = 3, n = 4.
+0 1 2 3
+1 2 3 4
+2 3 4 5 */
+
+/*
+Start();
+
+void Start()
+{
+    int rows = SetNumber("rows");
+    int columns = SetNumber("columns");
+
+    int[,] matrix = GetMatrix(rows, columns, 0, 10);
+    PrintMatrix(matrix);
+}
+
+int SetNumber(string numberName)
+{
+    Console.Write($"Enter number {numberName}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int[,] GetMatrix(int rows, int columns, int min, int max)
+{
+    int[,] array = new int[rows, columns];
+
+    var rand = new Random();
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
+
+    return array;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+
+}
+
+*/
+
+/* Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти 
+элементы на их квадраты.
+ */
+/*
+Start();
+
+void Start()
+{
+    int rows = SetNumber("rows");
+    int columns = SetNumber("columns");
+
+    int[,] matrix = GetMatrix(rows, columns, 0, 10);
+    PrintMatrix(matrix);
+    Console.WriteLine();
+    int[,] array = ChangeMatrix(matrix);
+    PrintMatrix(array);
+}
+
+
+int SetNumber(string numberName)
+{
+    Console.Write($"Enter number {numberName}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int[,] GetMatrix(int rows, int columns, int min, int max)
+{
+    int[,] array = new int[rows, columns];
+
+    var rand = new Random();
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = rand.Next(min, max + 1);
+        }
+    }
+
+    return array;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+
+}
+
+int[,] ChangeMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                matrix[i, j] = matrix[i, j] * matrix[i, j];
+            }
+
+        }
+    }
+
+    return matrix;
+}
+*/
+/*Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали 
+(с индексами (0,0); (1;1) и т.д.*/
+
+Start();
+
+void Start()
+{
+    int rows = SetNumber("rows");
+    int columns = SetNumber("columns");
+
+    int[,] matrix = GetMatrix(rows, columns, 0, 10);
+    PrintMatrix(matrix);
+
+    int result = GetDiagonalSum(matrix);
+    Console.WriteLine($"Сумма элементов главной диагонали = {result}");
+}
+
+
+int SetNumber(string numberName)
+{
+    Console.Write($"Enter number {numberName}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int[,] GetMatrix(int rows, int columns, int min, int max)
+{
+    int[,] array = new int[rows, columns];
+
+    var rand = new Random();
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = rand.Next(min, max + 1);
+        }
+    }
+
+    return array;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+
+}
+
+int GetDiagonalSum(int[,] matrix)
+{
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                sum += matrix[i,j];
+            }
+
+        }
+    }
+
+    return sum;
+}
