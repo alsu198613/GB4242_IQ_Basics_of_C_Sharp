4e2ed91f4cf921519fb62d61a102a6b8395ea2d5
alsu198613 <alsu198613@gmail.com>
2023-10-27 17:20:49 +0300

    update

---
+++ Seminar01/Program.cs
@@ -0,0 +1,222 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное 
+// на само себя).
+
+// Например:
+// 4 -> 16 
+// -3 -> 9 
+// -7 -> 49
+
+Console.WriteLine("Enter number : ");
+string strNum = Console.ReadLine();
+int num = Convert.ToInt32(strNum);
+
+int sqr = num * num;
+
+Console.WriteLine($"sqr = {sqr}");
+
+
+//Напишите программу, которая на вход принимает два числа и проверяет,
+//является ли первое число квадратом второго.
+
+Console.Clear();
+Console.Write("Введите A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+sqr = Convert.ToInt32(Math.Pow(numberB, 2));
+if (sqr == numberA)
+{
+    Console.WriteLine("A является квадратом B");
+}
+else
+{
+    Console.WriteLine("A не является квадратом B");
+}
+
+
+//Напишите программу, 
+//которая будет выдавать название дня недели по заданному номеру.
+
+Console.Clear();
+
+Console.Write("Введите номер дня недели: ");
+int numberDayOfWeek = Convert.ToInt32(Console.ReadLine());
+
+if (numberDayOfWeek == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else if (numberDayOfWeek == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else if (numberDayOfWeek == 4)
+{
+    Console.WriteLine("Среда");
+}
+else if (numberDayOfWeek == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if (numberDayOfWeek == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else if (numberDayOfWeek == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else if (numberDayOfWeek == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+else
+{
+    Console.WriteLine("Введен неправильный номер");
+}
+
+
+switch (numberDayOfWeek)
+{
+    case 1: Console.WriteLine("Понедельник"); break;
+    case 2: Console.WriteLine("Вторник"); break;
+    case 3: Console.WriteLine("Среда"); break;
+    case 4: Console.WriteLine("Четверг"); break;
+    case 5: Console.WriteLine("Пятница"); break;
+    case 6: Console.WriteLine("Суббота"); break;
+    case 7: Console.WriteLine("Воскресенье"); break;
+    default: Console.WriteLine("Введен неправильный номер"); break;
+
+}
+
+
+//Задача с двумя друзьями и собакой 
+
+int count = 0;
+int distance = 10000;
+int FirstFriendSpeed = 1;
+int SecondFriendSpeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+int time = 0;
+
+
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = distance / (FirstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance / (SecondFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+    distance = distance - (FirstFriendSpeed + SecondFriendSpeed) * time;
+    count++;
+}
+
+Console.WriteLine($"Количество раз, за которое собака пробежит, равно - {count}");
+
+
+//Напишите программу, которая на вход принимает два числа и выдаёт, 
+//какое число большее, а какое меньшее.
+
+Console.Clear();
+Console.Write("Введите A: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите B: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine("A больше B");
+}
+else if (number1 < number2)
+{
+    Console.WriteLine("B больше A");
+}
+else
+{
+    Console.WriteLine("A равно B");
+}
+
+
+
+
+
+
+// Задача 4: Напишите программу, которая принимает на вход 
+// три числа и выдает максимальное из этих чисел.
+
+Console.Clear();
+Console.Write("Введите первое чило: ");
+number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+number2 = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int number3 = int.Parse(Console.ReadLine());
+
+int max = number1; // 10
+
+
+if (max < number2)//-10
+{
+    max = number2;
+}
+if (max < number3)//-15
+{
+    max = number3;
+}
+
+Console.WriteLine($"максимальное число = {max} {number1} {number2} {number3} ");
+
+//Напишите программу, которая на вход принимает число и выдаёт,
+// является ли число чётным (делится ли оно на два без остатка).
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
+
+//Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+
+Console.Clear();
+Console.Write("Введите N: ");
+int numN = int.Parse(Console.ReadLine());
+
+//Первое решение
+int i = 1;
+while (i <= numN)//N=-15
+{
+    if (i % 2 == 0)
+    {
+        Console.Write($"{i} ");
+
+    }
+
+    i++;
+}
+Console.WriteLine();
+
+//Второе решение
+i = 2;
+while (i <= numN)
+{
+    Console.Write($"{i} ");
+    i += 5;
+    i = i + 5;
+
+}
+
---
+++ Seminar02/Program(2).cs
@@ -0,0 +1,189 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+// Например:
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+int num = new Random().Next(10, 100);
+
+int num1 = num / 10;
+int num2 = num % 10;
+
+Console.WriteLine(num);
+
+if (num1 > num2)
+{
+    Console.WriteLine($"num1 > num2 max = {num1} ");
+}
+else if (num1 < num2)
+{
+    Console.WriteLine($"num1 < num2  max = {num2} ");
+}
+else
+{
+    Console.WriteLine($"num1 == num2  max = {num1} ");
+}
+
+
+//GB HW
+//10. Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+
+num = new Random().Next(100, 1000);
+int secondNumber = num / 10 % 10;
+Console.WriteLine($"Вторая цифра в числе {num} -> {secondNumber}");
+
+//13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+//что третьей цифры нет.
+
+Console.Clear();
+Console.Write("Введите число: ");
+num = int.Parse(Console.ReadLine());
+if (num < 100)
+{
+    Console.WriteLine($"Третьей цифры нет");
+    return;
+}
+int thirdNumber = (num / 100) % 10;
+Console.WriteLine($"Третья цифра числа {num} -> {thirdNumber}");
+
+//15. Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+
+Console.Clear();
+Console.Write("Введите число: ");
+num = int.Parse(Console.ReadLine());
+if ((num > 7) || (num < 1))
+{
+    Console.WriteLine("Неверный день недели");
+    return;
+}
+if ((num > 5))
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+//end
+
+//My HW
+int SetNumber(string text = "")
+{
+    Console.Write($"Enter number {text}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//То что дала GeekBrains
+//645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+
+
+// Console.WriteLine("Enter index : ");
+// int index = Convert.ToInt32(Console.ReadLine());
+
+//double pow = Math.Pow(10, index);
+
+num = SetNumber();
+Console.Clear();
+
+if (num < 100)
+{
+    Console.WriteLine($"Цифра меньше индекса 3");
+
+}
+else
+{
+
+    int tmp = num;
+
+    while (tmp >= 1000)
+    {
+        tmp /= 10;
+    }
+
+    int result = tmp % 10;
+
+    Console.WriteLine($"Цифра под индексом 3 числа {num} -> {result}");
+}
+
+
+//15. Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+
+//1
+Console.WriteLine("Enter dayWeek : ");
+string strDayWeek = Console.ReadLine();
+
+PrintIsWeekend(strDayWeek);
+
+void PrintIsWeekend(string dayWeek)
+{
+
+    switch (dayWeek)
+    {
+        case "1":
+            Console.WriteLine("Понедельник- НЕТ");
+            break;
+        case "2":
+            Console.WriteLine("Вторник- НЕТ");
+            break;
+        case "3":
+            Console.WriteLine("Среда- НЕТ");
+            break;
+        case "4":
+            Console.WriteLine("Четверг- НЕТ");
+            break;
+        case "5":
+            Console.WriteLine("Пятница- Да");
+            break;
+        case "6":
+            Console.WriteLine("Суббота- Нет");
+            break;
+        case "7":
+            Console.WriteLine("Воскресенье- Нет");
+            break;
+
+    }
+
+}
+
+//2 
+
+bool ValidateWeekday(int dayWeek)
+{
+    return dayWeek >= 1 && dayWeek <= 7 ? true : false;
+}
+
+string PrintWeekendDay(int dayWeek)
+{
+    return dayWeek > 5 && dayWeek <= 7 ? "Weekend)))" : "Work day(((";
+}
+
+int weekDay = SetNumber("week day");
+
+if (ValidateWeekday(dayWeek))
+{
+    System.Console.WriteLine(PrintWeekendDay(weekDay));
+}
+else
+{
+    System.Console.WriteLine("Week day not valid()");
+}
+
+//end
+
+
+
+
+
